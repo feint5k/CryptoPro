03fb8069eb071bf45a6509e3da7baffb2228a410
feint5k <feint5k@proton.me>
2022-12-06 06:05:12 +0000

    correct order

--- app/src/androidTest/java/com/example/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/crypto/ExampleInstrumentedTest.kt
@@ -18,4 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().ta
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.crypto", appContext.packageName)
+    }
+}
