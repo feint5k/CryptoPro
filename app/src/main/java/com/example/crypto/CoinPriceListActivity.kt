16efd13aa77a925c9bfee88cd75e17ff4a10b41f
feint5k <feint5k@proton.me>
2022-12-08 17:38:33 +0000

    chore: update tests

--- app/src/main/java/com/example/crypto/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/crypto/CoinPriceListActivity.kt
@@ -9,4 +9,13 @@ class CoinPriceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.coin_price_list_activity)
-        supportFragme
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_holder, CoinPricefragment1.newInstance())
+            .addToBackStack(null)
+            .commit()
+
+             }
+
+
+    
