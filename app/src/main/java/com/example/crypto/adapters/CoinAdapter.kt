45c8f98fb5399fcb2226713aba3048fc33fd04a1
feint5k <feint5k@proton.me>
2022-12-29 07:21:21 +0000

    minor changes to README.md

--- app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
+++ app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
@@ -1,3 +1,8 @@
 package com.example.crypto.adapters
 
-import android.view.Layo
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.crypto.
