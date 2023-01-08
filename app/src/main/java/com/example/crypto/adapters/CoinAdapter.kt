f2a655d6bd7ec82819cb275afd4e0714d5040b8e
feint5k <feint5k@proton.me>
2023-01-08 10:33:41 +0000

    PNG

--- app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
+++ app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
@@ -5,4 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.crypto.
+import com.example.crypto.R
+import com.example.crypto.fragment.CoinPricefragment1
+import com.example.crypto.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.item_coin_info.view.*
