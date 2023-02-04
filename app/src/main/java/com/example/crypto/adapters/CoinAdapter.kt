f540f78b4a67e5668f61b7c5592a26854d156338
feint5k <feint5k@proton.me>
2023-02-04 12:06:24 +0000

    added MIT license file

--- app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
+++ app/src/main/java/com/example/crypto/adapters/CoinAdapter.kt
@@ -43,4 +43,8 @@ class CoinAdapter(private val context: CoinPricefragment1) :
             val symbolsTemplate = context.resources.getString(R.string.symbols_template)
             val lastTemplate = context.resources.getString(R.string.last_update_str)
             tvSymbols.text = String.format(symbolsTemplate, coin.FROMSYMBOL, coin.TOSYMBOL)
-            tvPrice.
+            tvPrice.text = coin.PRICE.toString()
+            tvUpdate.text = String.format(lastTemplate, coin.getFormatedTime())
+            Glide.with(context)
+                .load(coin.getFullImageUrl())
+                .into(ho
