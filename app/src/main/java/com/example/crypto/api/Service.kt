148ada56865948b7d2c79c141d66efde2b445f2c
feint5k <feint5k@proton.me>
2023-02-09 12:29:40 +0000

    colorize

--- app/src/main/java/com/example/crypto/api/Service.kt
+++ app/src/main/java/com/example/crypto/api/Service.kt
@@ -9,4 +9,6 @@ import retrofit2.http.Query
 interface Service {
     @GET("top/totalvolfull")
     suspend fun getTopCoinsInfo(
-        @Query(QUERY_PARAM_LIM
+        @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
+        @Query(QUERY_PARAM_TSIMBOL) tSym: String = QUERY_PARAM_CURRENCY,
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "bcd9a7f860cf
