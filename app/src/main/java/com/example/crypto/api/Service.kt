28e7a043dbf25b2df1484f69b12cb3ff76d7a38b
feint5k <feint5k@proton.me>
2023-02-09 14:59:00 +0000

    more doc

--- app/src/main/java/com/example/crypto/api/Service.kt
+++ app/src/main/java/com/example/crypto/api/Service.kt
@@ -11,4 +11,9 @@ interface Service {
     suspend fun getTopCoinsInfo(
         @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
         @Query(QUERY_PARAM_TSIMBOL) tSym: String = QUERY_PARAM_CURRENCY,
-        @Query(QUERY_PARAM_API_KEY) apiKey: String = "bcd9a7f860cf
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "bcd9a7f860cf051f79ac147c33e5d934029759a50e3aa9cd305708c9f8aaf90b"
+    ): CoinInfoListOfData
+
+    @GET("pricemultifull")
+    suspend fun getFullPriceList(
+        @Query(QUERY_PARAM_TSIMBO
