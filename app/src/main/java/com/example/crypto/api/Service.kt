6a52a8e05ad21dc0ca9a95e77e6e816f59e1278b
feint5k <feint5k@proton.me>
2023-02-09 08:56:37 +0000

    admin update

--- app/src/main/java/com/example/crypto/api/Service.kt
+++ app/src/main/java/com/example/crypto/api/Service.kt
@@ -2,4 +2,11 @@ package com.example.crypto.api
 
 import com.example.crypto.pojo.CoinInfoListOfData
 import com.example.crypto.pojo.CoinPriceRAW
-import io.reactiv
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Service {
+    @GET("top/totalvolfull")
+    suspend fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_LIM
