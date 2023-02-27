5359047bc48a995815e6368eea4ae49ce414416f
feint5k <feint5k@proton.me>
2023-02-27 12:46:41 +0000

    added env

--- app/src/main/java/com/example/crypto/database/AppDatabase.kt
+++ app/src/main/java/com/example/crypto/database/AppDatabase.kt
@@ -1,3 +1,9 @@
 package com.example.crypto.database
 
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.crypto.pojo.CoinPriceInfo
+
+@Database(entities = [CoinPriceInfo::class], version = 1, e
