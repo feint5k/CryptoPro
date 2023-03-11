2223c7ed32ac9f6a5494161c7c50117bfcb259a5
feint5k <feint5k@proton.me>
2023-03-11 14:01:44 +0000

    ChangeLog update

--- app/src/main/java/com/example/crypto/database/AppDatabase.kt
+++ app/src/main/java/com/example/crypto/database/AppDatabase.kt
@@ -11,4 +11,9 @@ abstract class AppDatabase : RoomDatabase() {
     companion object {
 
         private var db: AppDatabase? = null
-        private const val DB_NAME = "main.d
+        private const val DB_NAME = "main.db"
+        private val LOCK = Any()
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(LOCK) {
+                db?.let { return it }
+     
