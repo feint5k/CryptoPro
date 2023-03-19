aeccaf1982821ba8d98ace3f549fe9823b8b88a8
feint5k <feint5k@proton.me>
2023-03-19 09:43:51 +0000

    Some refactorings.

--- app/src/main/java/com/example/crypto/database/CoinPriceDAO.kt
+++ app/src/main/java/com/example/crypto/database/CoinPriceDAO.kt
@@ -2,4 +2,11 @@ package com.example.crypto.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import an
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.crypto.pojo.CoinPriceInfo
+
+@Dao
+interface CoinPriceDAO {
+    @Query("SELECT * FROM full_p
