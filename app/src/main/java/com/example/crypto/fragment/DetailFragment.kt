e759e45333650227f0dac676d14f2fe85914fef8
feint5k <feint5k@proton.me>
2023-03-22 14:41:29 +0000

    Minor formatting fix.

--- app/src/main/java/com/example/crypto/fragment/DetailFragment.kt
+++ app/src/main/java/com/example/crypto/fragment/DetailFragment.kt
@@ -3,4 +3,9 @@ package com.example.crypto.fragment
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
-im
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvi
