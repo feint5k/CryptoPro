07975b17e3152c49eb9a169f80545b0d841eb295
feint5k <feint5k@proton.me>
2023-04-19 08:22:19 +0000

    version 1.1

--- app/src/main/java/com/example/crypto/pojo/CoinPriceRAW.kt
+++ app/src/main/java/com/example/crypto/pojo/CoinPriceRAW.kt
@@ -2,4 +2,9 @@ package com.example.crypto.pojo
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-i
+import org.json.JSONObject
+
+data class CoinPriceRAW (
+    @SerializedName("RAW")
+    @Expose
+    val coinPriceInfoObject: Map<String,Map<String,CoinPriceInfo>>? = null
