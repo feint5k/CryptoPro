e06954816cc2e8fcd0ae8f41ab9c4e2cf6bcf0b5
feint5k <feint5k@proton.me>
2023-05-12 12:45:06 +0000

    Cleanup unused files

--- app/src/main/java/com/example/crypto/utils/TimeUtils.kt
+++ app/src/main/java/com/example/crypto/utils/TimeUtils.kt
@@ -5,4 +5,9 @@ import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun conve
+fun convertTime (timestamp:Long?):String {
+    if (timestamp == null) return ""
+    val stamp = Timestamp(timestamp * 1000)
+    val date = Date(stamp.time)
+    val pattern = "HH:mm:ss"
+    val sdf = SimpleDateFormat(pa
